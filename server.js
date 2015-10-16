3d0184604fff6dd04e75c3fc00f87b375cebdc28
mikey-pawell <mikeypawell@gmail.com>
2015-10-16 14:45:04 -0700

    initial express server

---
+++ server.js
@@ -0,0 +1,17 @@
+  // server.js
+  // SERVER SIDE JAVASCRIPT
+  var express = require('express');
+  var app = express();
+
+  app.get('/', function (req, res) {
+    res.send('Hello World!');
+  });
+
+
+
+
+
+  var server = app.listen(process.env.PORT || 3000, function () {
+    console.log('Example app listening at http://localhost:3000/');
+  });
+
